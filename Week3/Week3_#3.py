2842ca9d4d6a10d53f0dd64e94a6adeb7da24e17
Lughaide <19520504@gm.uit.edu.vn>
2021-03-12 00:01:42 +0700

    Finished

---
+++ Week3/Week3_#3.py
@@ -0,0 +1,30 @@
+n = list(map(int, input().split()))
+Arr1 = list(map(int, input().split()))
+Arr2 = list(map(int, input().split()))
+i = 0
+j = 0
+k = 0
+Arr3 = [None] * (n[0] + n[1])
+
+while (i < n[0]) & (j < n[1]):
+    if Arr1[i] < Arr2[j]:
+        Arr3[k] = Arr1[i]
+        i += 1
+        k += 1
+    else:
+        Arr3[k] = Arr2[j]
+        j += 1
+        k += 1
+
+while i < n[0]:
+    Arr3[k] = Arr1[i]
+    i += 1
+    k += 1
+
+while j < n[1]:
+    Arr3[k] = Arr2[j]
+    j += 1
+    k += 1
+
+
+print(*Arr3)
