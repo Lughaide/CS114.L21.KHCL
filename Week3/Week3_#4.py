2de06fccc0dfd668e7c387ab5beee1cb73c00b29
Lughaide <19520504@gm.uit.edu.vn>
2021-03-16 23:00:29 +0700

    Added credits.

--- Week3/Week3_#4.py
+++ Week3/Week3_#4.py
@@ -1,10 +1,10 @@
+#Thanks to khiemledev for the brilliant matrix idea
 h, w = map(int, input().split())
 data = [list(map(int, input().split())) for i in range(h)]
 top, left, bottom, right = map(int, input().split())
 for i in range(h):
     if (i < top) | (i > bottom):
         data[i] = [0] * w
-
     data[i][:left] = [0] * left
     data[i][right+1:] = [0] * (w - right - 1)
 for i in range(h):
