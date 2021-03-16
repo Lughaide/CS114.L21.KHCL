c2c398c0a15e5f5e821ddaaf42096e13ed77b233
Lughaide <19520504@gm.uit.edu.vn>
2021-03-16 22:58:58 +0700

    Finished

---
+++ Week3/Week3_#4.py
@@ -0,0 +1,11 @@
+h, w = map(int, input().split())
+data = [list(map(int, input().split())) for i in range(h)]
+top, left, bottom, right = map(int, input().split())
+for i in range(h):
+    if (i < top) | (i > bottom):
+        data[i] = [0] * w
+
+    data[i][:left] = [0] * left
+    data[i][right+1:] = [0] * (w - right - 1)
+for i in range(h):
+    print(*data[i])
