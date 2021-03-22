209166724cd8084c8dbed3b9eea32f46792de483
Lughaide <19520504@gm.uit.edu.vn>
2021-03-23 00:13:04 +0700

    Finished

---
+++ Actual Week3/Week3A_#7.py
@@ -0,0 +1,8 @@
+import math
+from sys import stdin, stdout
+
+n, m = stdin.readline().split()
+sum = 0
+length = int(math.pow(10, len(n)))
+sum = (int(m) - int(n)) // length + 1
+stdout.write(str(sum))
